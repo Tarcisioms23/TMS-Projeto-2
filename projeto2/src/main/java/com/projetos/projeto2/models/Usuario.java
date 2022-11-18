31b308f5bf1f8c35d60fc250e1c00afe50701912
Tarcisio <tarcisioms23@hotmail.com>
2022-11-18 10:34:34 -0300

    1 commit package models

---
+++ projeto2/src/main/java/com/projetos/projeto2/models/Usuario.java
@@ -0,0 +1,48 @@
+package com.projetos.projeto2.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario implements Serializable{
+
+	
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private int idUsuario;
+		private String nome;
+		private String senha;
+		
+		public int getIdUsuario() {
+			return idUsuario;
+		}
+		public void setIdUsuario(int idUsuario) {
+			this.idUsuario = idUsuario;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getSenha() {
+			return senha;
+		}
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+		
+		
+		
+}
